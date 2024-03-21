431a2eae0832c7dab72eab34352d9625368953c4
aatif shaikh <aatifshaikh4564@gmail.com>
2024-03-21 13:01:52 +0530

    Home page ProductItem changes

--- ostraders/src/components/ProductItem.js
+++ ostraders/src/components/ProductItem.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import styles from '../styles/product.module.css'
+import { Link } from 'react-router-dom'
 
 const ProductItem = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <>  
+            
+
+            <div className={styles.item}>
+                <Link><img src="./images/car1.jpg" className='img-fluid' alt="" /></Link>
+                <div className={styles.item_content}>
+                    <div className="d-flex justify-content-between align-items-center mt-2 mb-1">
+                        <Link><h4>Car Name</h4></Link>
+                        <span className={styles.price}>$20,000</span>
+                    </div>
+                    <p>Build Year: 2000</p>
+                    <div className={styles.details}>
+                        <i class="fa-solid fa-engine ms-0"></i> 5600 CC
+                        <i class="fa-solid fa-engine"></i> 5600 CC
+                        <i class="fa-solid fa-engine"></i> 5600 CC
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default ProductItem
--- ostraders/src/pages/Home.js
+++ ostraders/src/pages/Home.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProductItem from '../components/ProductItem'
+import styles from '../styles/product.module.css'
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
+
+  const [selectedItem, setSelectedItem] = useState('Luxury');
+
+  const handleClick = (itemName) => {
+    setSelectedItem(itemName);
+  };
+
+
   return (
     <>
-      <ProductItem />
+
+      <div className="container-fluid padd-x my-5">
+
+        <div className="heading text-center">
+          <h2>Our car collection</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quibusdam placeat excepturi omnis similique perferendis</p>
+        </div>
+
+        <div className={styles.product_nav}>
+          <ul>
+            <li className={selectedItem === 'Luxury' ? styles.current : ''} onClick={() => handleClick('Luxury')}>Luxury</li>
+            <li className={selectedItem === 'Premium' ? styles.current : ''} onClick={() => handleClick('Premium')}>Premium</li>
+            <li className={selectedItem === 'Electric' ? styles.current : ''} onClick={() => handleClick('Electric')}>Electric</li>
+            <li className={selectedItem === 'Sports' ? styles.current : ''} onClick={() => handleClick('Sports')}>Sports</li>
+          </ul>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-3">
+            <ProductItem />
+          </div>
+          <div className="col-lg-3">
+            <ProductItem />
+          </div>
+          <div className="col-lg-3">
+            <ProductItem />
+          </div>
+          <div className="col-lg-3">
+            <ProductItem />
+          </div>
+        </div>
+
+        <div className="text-center my-5">
+          <Link class="btn-12"><span>View More</span></Link>
+        </div>
+      </div>
     </>
   )
 }
